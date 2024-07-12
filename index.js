50a6cd7fef634a06a876eb43f123b0f820813ce8
nahashon <nashonmwendwa0@gmail.com>
2024-07-12 21:55:56 +0300

    arrow-functions

---
+++ index.js
@@ -0,0 +1,10 @@
+// 1) has a function expression called divide
+const divide = function() {
+    return 2000 / 100;
+  };
+  
+  // 3) has an arrow function called square
+  const square = x => x * x;
+  
+  // 5) has an arrow function called add
+  const add = (x, y) => x + y;
